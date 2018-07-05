e125e224488c76fcaad51b352bed7a4134c802a8
Matthew Dean <matthew-dean@users.noreply.github.com>
2018-07-04 22:05:41 -0700

    Add SVG function

---
+++ index.js
@@ -0,0 +1,45 @@
+function htmlAttr(decl) {
+  return decl.name[0].value + '="' + decl.value.toCSS() + '" ';
+}
+
+var nodeStack = [];
+
+module.exports = {
+  install: function(less, pluginManager, functions) {
+    functions.add('svg', function(arg) {
+      var rules;
+      if (arg.ruleset) {
+        rules = arg.ruleset.rules;
+      } else if (arg.rules) {
+        rules = arg.rules;
+      } else if (Array.isArray(arg)) {
+        rules = arg;
+      } else {
+        rules = [arg];
+      }
+      var str = '<svg xmlns="http://www.w3.org/2000/svg" ';
+      nodeStack.push('svg');
+
+      function addRules(rules) {
+        rules.forEach(function(rule) {
+          if (rule instanceof less.tree.Declaration) {
+            str += htmlAttr(rule);
+          } else {
+            var sel = rule.selectors[0].toCSS().trim();
+            str += '><' + sel + ' ';
+            nodeStack.push(sel);
+            addRules(rule.rules);
+          }
+        });
+        str += '></' + nodeStack.pop();
+      }
+
+      addRules(rules);
+      str += '>';
+      var returner, index = arg.getIndex(), fileInfo = arg.fileInfo();
+
+      returner = 'data:image/svg+xml,' + encodeURIComponent(str);
+      return new less.tree.URL(new less.tree.Quoted('\'' + returner + '\'', returner, false, index, fileInfo), index, fileInfo);
+    })
+  }
+}
